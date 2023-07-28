37f2e2296eb22a37ef12ec4b45558e4ea40539c0
Dworv <dwarvyt@gmail.com>
2023-07-27 21:04:11 -0700

    feat: moving asteroids

--- src/obstacle.rs
+++ src/obstacle.rs
@@ -5,7 +5,7 @@ pub struct ObstaclePlugin;
 
 impl Plugin for ObstaclePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, add_obstacles);
+        app.add_systems(Update, (add_obstacles, move_obstacles));
     }
 }
 
@@ -18,8 +18,18 @@ fn add_obstacles(mut commands: Commands, asset_server: Res<AssetServer>) {
             Obstacle,
             SpriteBundle {
                 texture: asset_server.load("textures/meteor.png"),
+                transform: Transform {
+                    translation: Vec3 { x: 10., y: thread_rng().gen_range(-100.0..100.0), z: 0. },
+                    ..default()
+                },
                 ..default()
             }
         ));
+    }
+}
+
+fn move_obstacles(mut query: Query<(&mut Obstacle, &mut Transform)>) {
+    for (_, mut t) in &mut query {
+        t.translation.x -= 10.;
     }
 }
