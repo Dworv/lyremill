b513bd1c8bdcf855b79479d1630823bd73e1247c
Dworv <dwarvyt@gmail.com>
2023-07-29 12:34:02 -0700

    ref: change how speed works

--- src/player.rs
+++ src/player.rs
@@ -4,22 +4,30 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_player)
-            .add_systems(Update, update_player);
+        app
+            .insert_resource(DeltaMovement(0.))
+            .insert_resource(PlayerActions::default())
+            .add_systems(Startup, setup_player)
+            .add_systems(Update, (read_player_actions, calculate_speed).chain());
     }
 }
 
-#[derive(Resource)]
+#[derive(Deref, Resource)]
 pub struct DeltaMovement(f32);
+
+#[derive(Default, Resource)]
+pub struct PlayerActions{
+    vert: f32,
+    thrust: f32,
+    gun: bool
+}
 
 #[derive(Component)]
 pub struct Player {
-    pub speed: f64,
-    pub offset: Vec3,
+    pub speed: f32,
 }
 
 fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(DeltaMovement(5.));
     commands.spawn((
         SpriteBundle {
             texture: asset_server.load("textures/plane.png"),
@@ -30,26 +38,42 @@ fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         },
         Player {
-            speed: 1.,
-            offset: Vec3::splat(0.),
+            speed: 0.,
         },
     ));
 }
 
-fn update_player(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Player, &mut Transform)>,
-) {
-    let mut movement = 0.;
-    if keyboard_input.pressed(KeyCode::W) {
-        movement += 1.;
-    } else if keyboard_input.pressed(KeyCode::S) {
-        movement -= 1.;
+fn read_player_actions(keyboard_input: Res<Input<KeyCode>>, mut actions: ResMut<PlayerActions>) {
+    actions.thrust = 0.;
+    if keyboard_input.pressed(KeyCode::A) {
+        actions.thrust -= 1.;
     }
+    if keyboard_input.pressed(KeyCode::D) {
+        actions.thrust += 1.;
+    }
+    actions.vert = 0.;
+    if keyboard_input.pressed(KeyCode::W) {
+        actions.vert -= 1.;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        actions.vert += 1.;
+    }
+    if keyboard_input.pressed(KeyCode::Space) {
+        actions.gun = true;
+    }
+}
 
-    let (mut player, mut transform) = query.get_single_mut().unwrap();
-    let distance = movement - player.offset.y;
-    let y_adjustment = distance / 16.;
-    player.offset.y += y_adjustment;
-    transform.translation.y += y_adjustment * 100.;
+const ACCEL: f32 = 1.;
+const MIN_SPEED: f32 = 100.;
+const MAX_SPEED: f32 = 1000.;
+
+fn calculate_speed(
+    player_actions: Res<PlayerActions>,
+    time: Res<Time>,
+    mut movement: ResMut<DeltaMovement>,
+    mut query: Query<(&mut Player)>,
+) {
+    let (mut player) = query.get_single_mut().unwrap();
+    player.speed += (player_actions.thrust * ACCEL * time.delta_seconds()).min(0.).max(1.);
+    movement.0 = MIN_SPEED + player.speed * (MAX_SPEED - MIN_SPEED);
 }
